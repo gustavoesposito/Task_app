2680af2ff0ac0c371e6e5af950f96d76ca3d84ed
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-10-31 11:36:19 -0300

    att

--- task/App.js
+++ task/App.js
@@ -1,15 +1,19 @@
  import { NavigationContainer } from '@react-navigation/native';
  import {   createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import Task from './src/pages/Task';
 
 const Stack = createStackNavigator
 
 export default function App() {
   return (
    <NavigationContainer>
-    <Stack.Navigator>
-      
-    </Stack.Navigator>
+    <Stack.Navigator initialRouteName="Task">
+       <Stack.Screen
+       name="Task"
+       component={Task}
+       />
+        </Stack.Navigator>
    </NavigationContainer>
   );
 }
