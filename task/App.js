0540c0f881b1791306dc51f41d87128071a6f81a
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-10-31 11:27:50 -0300

    att

--- task/App.js
+++ task/App.js
@@ -1,5 +1,8 @@
- 
+ import { NavigationContainer } from '@react-navigation/native';
+ import {   createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+
+const Stack = createStackNavigator
 
 export default function App() {
   return (
