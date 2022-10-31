dee6b2d260f40e6acd6ea1e7bd9d0d52d1c151f6
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-10-31 11:44:57 -0300

    att

--- task/App.js
+++ task/App.js
@@ -2,6 +2,9 @@
  import {   createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import Task from './src/pages/Task';
+import NewTask from './src/pages/NewTask';
+import Details from './src/pages/Details';
+import { StyleSheet } from 'react-native';
 
 const Stack = createStackNavigator
 
@@ -12,6 +15,23 @@ export default function App() {
        <Stack.Screen
        name="Task"
        component={Task}
+       options={{
+        headerTintColor:"f92e6a"
+       }}
+       />
+       <Stack.Screen
+       name="NewTask"
+       component={NewTask}
+       options={{
+        headerTintColor:"f92e6a"
+       }}
+       />
+       <Stack.Screen
+       name="Details"
+       component={Details}
+       options={{
+        headerTintColor:"f92e6a"
+       }}
        />
         </Stack.Navigator>
    </NavigationContainer>
---
+++ task/src/pages/Details/index.js
@@ -0,0 +1,10 @@
+import  React from 'react'
+import {View, Text} from "react-native"
+
+export default function Details(){
+    <View>
+        <Text>
+            Page Details
+        </Text>
+    </View>
+}
